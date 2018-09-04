2fc6e9d0fc0c784117906bd3d6a190a37cda0899
WhiteToBlack <mertc90@gmail.com>
2018-09-04 22:36:27 +0300

    test message

--- nodeStarter.js
+++ nodeStarter.js
@@ -1,1 +1,13 @@
-New Git Project Added!
+
+var httpSunucu = require('http');
+
+var httpSunucu = httpSunucu.createServer(function(talep, cevap){
+	cevap.writeHead(
+		200, 
+		{"Content-Type" : "text/html;charset=utf-8"}
+	);
+	cevap.end("<h1>Hoş geldiniz!</h1><br /><b>node.Js sunucusu üzerinde çalışıyorsun. Kolay gelsin ;)</b>");
+});
+
+httpSunucu.listen(1453);
+console.log("Sunucu 1453 portunda başlatıldı.");
