1f73066df8ef43d03d03225a1202f2fe24b11187
WhiteToBlack <mertc90@gmail.com>
2018-09-04 23:44:43 +0300

    commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var url = require('url');
+var mssql = require('mssql');
+
+var renderHTML = function (path, response) {
+    fs.readFile(path, null, function (err, data) {
+        if (err) {
+            response.writeHead(404);
+            response.write("PathNotFound");
+        } else {
+            response.write(data);
+        }
+
+        response.end();
+    });
+}
+
+module.exports = {
+    handleRequest: function (request, response) {
+        response.writeHead(200, { 'Content-Type': "text/html" });
+        var path = url.parse(request.url).pathname;
+        switch (path) {
+            case '/':
+                renderHTML('./index.html', response);
+                break;
+            case '/login':
+                renderHTML('./login.html', response);
+                break;
+            default:
+                response.writeHead(404);
+                response.write("Route Not Found!");
+                response.end();
+                break;
+        }
+    },
+    getNodeProperties:function(){
+        async () => {
+            try {
+                const pool = await sql.connect('mssql://sa:123@./NodeJs')
+                const result = await sql.query`select * from NodeProperties`
+                console.dir(result)
+            } catch (err) {
+                // ... error checks
+            }
+        }
+
+    }
+};
